f2f450680b81fd3c7cca07ffa97183865e346d68
avinashjairam <avinash.jairam@gmail.com>
2017-06-14 08:48:06 -0400

    changed the way the linked list stack worked. Instead of adding elements at the back of the list, i add them to the front. Instead of popping elements from te back of the list, I am popping them from the front

--- STACK WITH LINKED LIST/StackLinkedList.java
+++ STACK WITH LINKED LIST/StackLinkedList.java
@@ -19,16 +19,20 @@ public class StackLinkedList{
 			head = new Node(data);
 			return;
 		}
+
+		Node temp = head;
+		head = newNode;
+		head.next = temp; 
 		
-		Node last = head;		//Otherwise, we need to move to the end of the list to add the node
-		newNode.next = null;    //Setting the next reference of the new node to null since it is at the back of the list
+		// Node last = head;		//Otherwise, we need to move to the end of the list to add the node
+		// newNode.next = null;    //Setting the next reference of the new node to null since it is at the back of the list
 
-		//Traverse the list to get to the end
-		while(last.next != null){
-			last = last.next;		//Moving on the next node in the list 
-		}
+		// //Traverse the list to get to the end
+		// while(last.next != null){
+		// 	last = last.next;		//Moving on the next node in the list 
+		// }
 
-		last.next = newNode; //Setting the next pointer of the last node in the list to the new node
+		// last.next = newNode; //Setting the next pointer of the last node in the list to the new node
 	}
 
 	//The pop method works in a Last In First Out Fashion
@@ -36,32 +40,37 @@ public class StackLinkedList{
 		
 		int x = -1; //Be default, the value to be removed is -1
 
-		//The two Node objects last and prev are need to find the last element in the list 
-		//Initially last is set to the head of the list and prev is set to null
-		Node last = head; 
-		Node prev = null;
+		// //The two Node objects last and prev are need to find the last element in the list 
+		// //Initially last is set to the head of the list and prev is set to null
+		// Node last = head; 
+		// Node prev = null;
 
-		//If last = null then the stack is empty. 
-		if(last == null){ 
-			return -1;
-		}
+		// //If last = null then the stack is empty. 
+		// if(last == null){ 
+		// 	return -1;
+		// }
 
-		//While the next node in the list is not last, set prev to the current node
-		//(last) and set the current node (last ) to the next node
-		while(last.next != null){
-		 	prev = last;
-		 	last = last.next;
-		 }
+		// //While the next node in the list is not last, set prev to the current node
+		// //(last) and set the current node (last ) to the next node
+		// while(last.next != null){
+		//  	prev = last;
+		//  	last = last.next;
+		//  }
 
-		 //Setting last node in the list to the penultimate node
-		last = prev;
-		//Assigning the value of the last node in the list to X. This is the node to be removed
-		x = last.next.data;
-		//Setting the last value to null
-		last.next = null;
+		//  //Setting last node in the list to the penultimate node
+		// last = prev;
+		// //Assigning the value of the last node in the list to X. This is the node to be removed
+		// x = last.next.data;
+		// //Setting the last value to null
+		// last.next = null;
 
-		//Decrementing the count
+		// //Decrementing the count
 		count--;
+
+		Node temp = head;
+		x = temp.data;
+		head = temp.next;
+
 		return x;
 	}
 
@@ -75,13 +84,13 @@ public class StackLinkedList{
 			return -1;
 		}
 		else{
-			Node last = head;
+			// Node last = head;
 
-			while(last.next != null){
-				last = last.next;
-			}
-			
-			return last.data;
+			// while(last.next != null){
+			// 	last = last.next;
+			// }
+
+			 return head.data;
 		}
 	}
 
